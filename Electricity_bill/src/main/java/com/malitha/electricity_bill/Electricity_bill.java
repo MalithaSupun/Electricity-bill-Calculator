7487ad2033f262638b2f178f2a59ffcafece5a46
Malitha supun <86181484+MalithaSupun@users.noreply.github.com>
2023-11-05 13:03:29 +0530

    Add files via upload

---
+++ Electricity_bill/src/main/java/com/malitha/electricity_bill/Electricity_bill.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.malitha.electricity_bill;
+
+/**
+ *
+ * @author malitha
+ */
+public class Electricity_bill {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
